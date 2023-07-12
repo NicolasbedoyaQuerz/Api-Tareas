892c6558a1b06860447a251968e8ae5a9a3dc118
Nicolas Bedoya <nicolasbedoya32@gmail.com>
2023-07-12 02:33:07 -0500

    agregando dotenv

--- src/utils/database.js
+++ src/utils/database.js
@@ -7,8 +7,8 @@ const db = new Sequelize({
     database: process.env.DB_DATABASE,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    dialect: 'postgress',
-    dialectOptions: {ssl: {require: true, rejectUnauthorized: false}},
+    dialect: 'postgres',
+   
 });
 
 module.exports = db;
